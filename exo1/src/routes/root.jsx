6dac6acafae749bd77a4e26a50dd5bc8b72ef26a
G11 <g11@ljouvet.local>
2024-03-11 15:19:41 +0100

    added search function
    Signed-off-by: G11 <g11@ljouvet.local>

--- exo1/src/routes/root.jsx
+++ exo1/src/routes/root.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
 import db from '../db.json'
 
 export default function Root() {
+    const [search, setSearch] = useState("")
+
+    function isSearched(name) {
+        let re = new RegExp(search,"i")
+        if (search.length === 0 || re.test(name) > 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
     let toKeys = Object.keys(db)
     let toArray = []
     toKeys.forEach((element) => {toArray.push(db[element])})
     return (
         <>
             <h1>Accurate encyclopedia</h1>
+            <input type="text" placeholder='search' value={search} onChange={(e) => {setSearch(e.target.value)}} />
             {toArray.map((element,i) => {
-                return <ProfileCard key={i} name={element.name} picture={element.picture} address={toKeys[i]} />
+                if (isSearched(element.name)) {
+                    return <ProfileCard key={i} name={element.name} picture={element.picture} address={toKeys[i]} />
+                }
             })}
         </>
     )
